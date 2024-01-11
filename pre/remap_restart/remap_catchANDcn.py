06aeabd6f487e60cc0dcbdbcb3962fc23b03c4fc
Rolf Reichle <rolf.reichle@nasa.gov>
2024-01-11 17:06:39 -0500

    clarified "land_only" option for remap_restarts.py package (remap_catchANDcn.py)

--- pre/remap_restart/remap_catchANDcn.py
+++ pre/remap_restart/remap_catchANDcn.py
@@ -410,7 +410,7 @@ def ask_catch_questions():
    return answers
 
 def remap_land_only():
-   print("\n It remaps catcment(cn) restarts with GLOBAL domain only. \n")
+   print("\n Remap land (catch[cn]) restart only; global domain only; ens0000 only. \n")
    answers = ask_catch_questions()
    config = get_config_from_answers(answers, config_tpl = True)
    config_yaml = config['output']['shared']['out_dir']+'/remap_params.yaml'
